bb839019d04608a548908073f044082b2f4aa855
adams <adams@adamsdeMBP.lan>
2019-03-06 08:24:22 +0800

    create

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM alpine:edge
+
+LABEL maintainer="Ghostry <ghostry.green@gmail.com>"
+
+RUN apk add --no-cache aria2
+
+ADD start.sh /start.sh
+ADD aria2.conf /aria2.conf
+
+RUN chmod +x /start.sh
+
+WORKDIR /
+
+VOLUME ["/aria2"]
+
+EXPOSE 6800
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -f /aria2/conf/aria2.conf ]; then
+	mkdir /aria2/conf
+	mkdir /aria2/down
+	cp /aria2.conf /aria2/conf/aria2.conf
+	if [ $SECRET ]; then
+		echo "rpc-secret=${SECRET}" >> /aria2/conf/aria2.conf
+	fi
+    touch /aria2/logs.txt
+fi
+aria2c --conf-path=/aria2/conf/aria2.conf --log=/aria2/logs.txt
