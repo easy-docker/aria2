2628b8fba1578df493aa09647a9709f54c527611
ghostry <ghostry@ghostry.cn>
2019-03-06 12:33:44 +0800

    增加创建文件

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docker-aria2
 通用的Aria2docker
+
+# 使用
+```
+docker run -d \
+--name garia2 \
+-p 6800:6800 \
+-v /data/aria2:/aria2 \
+-e SECRET=YOUR_SECRET_CODE \
+ghostry/aria2
+```
+其中SECRET可选,
+
--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ if [ ! -f /aria2/conf/aria2.conf ]; then
 	if [ $SECRET ]; then
 		echo "rpc-secret=${SECRET}" >> /aria2/conf/aria2.conf
 	fi
+    touch /aria2/conf/save-session.list
     touch /aria2/logs.txt
 fi
 aria2c --conf-path=/aria2/conf/aria2.conf --log=/aria2/logs.txt
